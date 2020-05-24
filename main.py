wrong order norm for flattened matrices

main.py
@@ -148,11 +148,11 @@ def run_trial_with_set_parameters(batch_size=128, num_iterations=200, model=None
                 # ----------------------------
 
                 # ----- WEIGHT TRACKING ---------
-                weight_norms = np.array([np.linalg.norm(p.data.flatten().numpy(), ord='fro') for p in model.parameters()])
+                weight_norms = np.array([np.linalg.norm(p.data.flatten().numpy()) for p in model.parameters()])
                 prod_weight_norm = np.prod(weight_norms)
                 prod_weight_norm_lst.append(prod_weight_norm)
 
-                inv_weight_norm_params = np.array([1 + 1.0/(np.linalg.norm(p.data.flatten().numpy(), ord='fro')) for p in model.parameters()])
+                inv_weight_norm_params = np.array([1 + 1.0/(np.linalg.norm(p.data.flatten().numpy())) for p in model.parameters()])
                 inv_weight_norm = np.prod(inv_weight_norm_params)
                 inv_norm_lst.append(inv_weight_norm)
 