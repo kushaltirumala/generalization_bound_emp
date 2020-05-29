df8c4d2d7c558eae037530bf3edcf3898e6a0751
Kushal Tirumala <kushaltirumala99@gmail.com>
2020-05-29 00:59:07 -0700

    running new high octane mnist

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ def load_model(num_classes_to_predict):
     high_n_units = [32*32*3, 2048, 512, 256, num_classes_to_predict]
 
     low_n_units_mnist = [28*28, 256, num_classes_to_predict]
-    high_n_units_mnist = [28*28, 2048, 1024, 512, 256, num_classes_to_predict]
-    model = MLP(low_n_units)
+    high_n_units_mnist = [28*28, 512, 256, 128, 64, num_classes_to_predict]
+    model = MLP(high_n_units_mnist)
     return model
 
 def get_norm_of_tensor(m, np_arr=False):
@@ -221,4 +221,4 @@ def run_trial_with_set_parameters(batch_size=128, num_iterations=200, model=None
 
 if __name__ == "__main__":
     mlp_model = load_model(10)
-    run_trial_with_set_parameters(batch_size=512, num_iterations=6000, model=mlp_model, lr=0.01, dataset_name="CIFAR10")
+    run_trial_with_set_parameters(batch_size=250, num_iterations=1000, model=mlp_model, lr=0.01, dataset_name="MNIST")
