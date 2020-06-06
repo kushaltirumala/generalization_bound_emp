99c5c1a277d9e356589a7866e722f2666ec6f41c
Kushal Tirumala <kushaltirumala99@gmail.com>
2020-06-06 03:11:03 -0700

    removing retrieve data

--- retrieve_data.sh
+++
@@ -1,4 +0,0 @@
-mkdir experiment_save/$1
-echo "Making directory in $1"
-scp -r ktirumal@ml-login3.cms.caltech.edu:/home/ktirumal/weight_norm_noise/experiment_save experiment_save/$1
-scp ktirumal@ml-login3.cms.caltech.edu:/home/ktirumal/weight_norm_noise/*.png experiment_save/$1
